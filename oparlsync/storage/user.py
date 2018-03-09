fd2caa822acdc30f9708340a9d8c6bb90da891c5
Ernesto Ruge <mail@ernestoruge.de>
2018-03-09 22:59:04 +0100

    User Reference Bugfixes

--- oparlsync/storage/user.py
+++ oparlsync/storage/user.py
@@ -10,18 +10,9 @@ Redistribution and use in source and binary forms, with or without modification,
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-from werkzeug.security import check_password_hash
-from passlib.hash import bcrypt
 from datetime import datetime
-from itsdangerous import URLSafeTimedSerializer
-from flask_mail import Message
-from hashlib import sha256
-from flask_login import login_user
-from ..extensions import mail
-from flask import current_app, render_template
-from mongoengine import Document, BooleanField, ReferenceField, DateTimeField, StringField, ListField, \
-    DecimalField, EmailField, IntField, GeoJsonBaseField
-
+from mongoengine import Document, BooleanField, ReferenceField, DateTimeField, StringField, ListField, DecimalField, \
+    EmbeddedDocument
 
 class User(Document):
     created = DateTimeField(default=datetime.utcnow())
