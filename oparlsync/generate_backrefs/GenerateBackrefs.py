68c40178e87f3d9867aa4829c82f19f774efab59
Ernesto Ruge <mail@ernestoruge.de>
2019-01-26 07:24:28 +0000

    prevent timeout at generating backrefs

--- oparlsync/generate_backrefs/GenerateBackrefs.py
+++ oparlsync/generate_backrefs/GenerateBackrefs.py
@@ -60,7 +60,7 @@ class GenerateBackrefs(BaseTask):
 
     def backref_single(self, obj_name, attr, backref_attr):
         filter = {'body': self.body, attr + '__exists': True}
-        for obj in obj_name.objects(**filter).no_cache().all():
+        for obj in obj_name.objects(**filter).timeout(False).no_cache().all():
             if obj != getattr(getattr(obj, attr), backref_attr):
                 setattr(getattr(obj, attr), backref_attr, obj)
                 getattr(obj, attr).save()
@@ -68,7 +68,7 @@ class GenerateBackrefs(BaseTask):
 
     def backref_item(self, obj_name, attr, backref_attr):
         filter = {'body': self.body, attr + '__exists': True}
-        for obj in obj_name.objects(**filter).no_cache().all():
+        for obj in obj_name.objects(**filter).timeout(False).no_cache().all():
             if obj not in getattr(getattr(obj, attr), backref_attr):
                 getattr(getattr(obj, attr), backref_attr).append(obj)
                 getattr(obj, attr).save()
@@ -76,7 +76,7 @@ class GenerateBackrefs(BaseTask):
 
     def backref_list(self, obj_name, attr, backref_attr):
         filter = {'body': self.body, attr + '__exists': True}
-        for obj in obj_name.objects(**filter).no_cache().all():
+        for obj in obj_name.objects(**filter).timeout(False).no_cache().all():
             for sub_obj in getattr(obj, attr):
                 if obj not in getattr(sub_obj, backref_attr):
                     getattr(sub_obj, backref_attr).append(obj)
@@ -85,7 +85,7 @@ class GenerateBackrefs(BaseTask):
 
     def backref_list_single(self, obj_name, attr, backref_attr):
         filter = {'body': self.body, attr + '__exists': True}
-        for obj in obj_name.objects(**filter).no_cache().all():
+        for obj in obj_name.objects(**filter).timeout(False).no_cache().all():
             for sub_obj in getattr(obj, attr):
                 if obj != getattr(sub_obj, backref_attr):
                     setattr(sub_obj, backref_attr, obj)
